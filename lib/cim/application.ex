552646a2579d6d68c78ed204fe271dd345fb4a08
Josh <josh.jacobsen@cogo.co>
2022-10-27 15:35:53 +1300

    Validate port is valid argument

--- lib/cim/application.ex
+++ lib/cim/application.ex
@@ -29,9 +29,30 @@ defmodule Cim.Application do
   defp port() do
     port = Application.fetch_env!(:cim, :port)
 
-    case String.to_integer(port) do
-      value ->
-        value
+    with {:ok, value} <- valid_port(port),
+         {:ok, port_as_integer} <- convert_to_integer(value) do
+      port_as_integer
+    else
+      _ ->
+        raise ArgumentError, message: "#{port} is not a valid integer value"
+    end
+  end
+
+  defp valid_port(port) do
+    if Regex.match?(~r{\A\d*\z}, port) do
+      {:ok, port}
+    else
+      {:error, :invalid_port}
+    end
+  end
+
+  defp convert_to_integer(port) do
+    case Integer.parse(port) do
+      {value, _} ->
+        {:ok, value}
+
+      :error ->
+        {:error, :invalid_integer}
     end
   end
 
